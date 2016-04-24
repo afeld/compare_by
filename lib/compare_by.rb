da0a2ddcb186aecf729fcb00bc4c5969554625b0
Aidan Feldman <aidan.feldman@gsa.gov>
2016-04-24 19:19:04 -0400

    use better variable names

--- lib/compare_by.rb
+++ lib/compare_by.rb
@@ -2,16 +2,16 @@ require 'compare_by/version'
 
 module CompareBy
   def self.included base
-    base.class_variable_set(:@@compare_by, nil)
+    base.class_variable_set(:@@comparison_attrs, nil)
     base.send :include, InstanceMethods
     base.extend ClassMethods
   end
 
   module InstanceMethods
     def ==(other)
-      compare_by = self.class.class_variable_get(:@@compare_by)
-      if compare_by
-        compare_by.all? do |attr_name|
+      attrs = self.class.class_variable_get(:@@comparison_attrs)
+      if attrs
+        attrs.all? do |attr_name|
           send(attr_name) == other.send(attr_name)
         end
       else
@@ -22,10 +22,10 @@ module CompareBy
     alias_method :eql?, :==
 
     def hash
-      compare_by = self.class.class_variable_get(:@@compare_by)
-      if compare_by
+      attrs = self.class.class_variable_get(:@@comparison_attrs)
+      if attrs
         # TODO use all values...?
-        send(compare_by.first).hash
+        send(attrs.first).hash
       else
         super
       end
@@ -34,7 +34,7 @@ module CompareBy
 
   module ClassMethods
     def compare_by(*attr_names)
-      class_variable_set(:@@compare_by, attr_names)
+      class_variable_set(:@@comparison_attrs, attr_names)
     end
   end
 end
